aafb6856ab31f3c7b17bcfca447a59c1f9b6bcd0
joseivanjunior <joseivanjunior@gmail.com>
2020-09-04 11:58:59 -0300

    Adicionar Procfile

---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+import { FiShoppingBag } from 'react-icons/fi'
+import './styles.css'
+
+import logo from '../../assets/book.svg'
+
+export default function Header() {
+    const cartSize = useSelector(state => state.cart.length)
+    return (
+        <header className="header">
+            <Link to="/" className="logo">
+                <img className="logo-icon" src={logo} alt="book" />
+                <span className="logo-text">Books</span>
+            </Link>
+
+            <Link to="/cart" className="header-cart">
+                <div>
+                    <strong>Sacola</strong>
+                    <span>
+                        <strong>{cartSize}</strong> livros
+                    </span>
+                </div>
+                <FiShoppingBag size={36} color="#FFF" />
+            </Link>
+        </header>
+    )
+}
